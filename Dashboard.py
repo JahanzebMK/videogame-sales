78f49c4a1b5c041f269c5f6d1237e78bebe5f4c0
JahanzebMK <j.khan360@outlook.com>
2023-12-27 04:21:42 +0000

    Updating to inc dashboard

--- Dashboard.py
+++ Dashboard.py
@@ -7,14 +7,15 @@ import plost
 vg_sales = pd.read_csv('https://raw.githubusercontent.com/JahanzebMK/videogame-sales/main/Data/scraped_vgsales.csv')
 
 # Add total sales column and convert year column to int dtype
-vg_sales['Sales'] = vg_sales['NA_Sales']+vg_sales['EU_Sales']+vg_sales['JP_Sales']+vg_sales['Other_Sales']
+vg_sales = vg_sales.rename(columns={c: c+' (millions)' for c in vg_sales.columns if c in ['NA_Sales', 'EU_Sales', 'JP_Sales', 'Other_Sales']})
+vg_sales['Global Sales (millions)'] = vg_sales['NA_Sales (millions)']+vg_sales['EU_Sales (millions)']+vg_sales['JP_Sales (millions)']+vg_sales['Other_Sales (millions)']
 vg_sales['Year']=vg_sales['Year'].astype(int)
 
 # Config layout of page and add important info
 st.set_page_config(layout='wide', initial_sidebar_state='expanded')
 st.sidebar.header('Video Game Sales (1980-2020)')
 st.sidebar.subheader('By Jahanzeb Khan')
-st.sidebar.markdown('Data obtained from vgchartz.com  \nSales values displayed in millions')
+st.sidebar.markdown('Data obtained from vgchartz.com')
 
 # Double slider to allow user to select which years to focus on
 st.sidebar.subheader('Year Slider')
@@ -60,22 +61,22 @@ vg_sales = vg_sales[vg_sales['Publisher'].isin(selected_publishers)]
     
 
 # Create aggregates of data depending on user selections to be used for visualisations
-platform_sales = vg_sales.groupby(['Platform'])['Sales'].agg('sum').reset_index()
-publisher_sales = vg_sales.groupby(['Publisher'])['Sales'].agg('sum').reset_index()
-genre_sales = vg_sales.groupby(['Genre'])['Sales'].agg('sum').reset_index()
-market_vg_sales = vg_sales.groupby(['Year'])[['NA_Sales', 'EU_Sales', 'JP_Sales', 'Other_Sales']].agg('sum').reset_index()
+platform_sales = vg_sales.groupby(['Platform'])['Global Sales (millions)'].agg('sum').reset_index()
+publisher_sales = vg_sales.groupby(['Publisher'])['Global Sales (millions)'].agg('sum').reset_index()
+genre_sales = vg_sales.groupby(['Genre'])['Global Sales (millions)'].agg('sum').reset_index()
+market_vg_sales = vg_sales.groupby(['Year'])[['NA_Sales (millions)', 'EU_Sales (millions)', 'JP_Sales (millions)', 'Other_Sales (millions)']].agg('sum').reset_index()
 market_vg_sales.Year = market_vg_sales.Year.astype(str)
 
 # Seperate visualisations into columns
 c1, c2, c3,c4 = st.columns((2.5,2.5,2.5,2.5))
 with c1:
     st.markdown('### Top 10')
-    st.dataframe(vg_sales.head(10), hide_index = True, column_order=("Name", "Platform", "Sales")) 
+    st.dataframe(vg_sales.head(10), hide_index = True, column_order=("Name", "Platform", "Global Sales (M)")) 
 with c2:
     st.markdown('### Platform Donut Chart')
     plost.donut_chart(
         data=platform_sales,
-        theta='Sales',
+        theta='Global Sales (millions)',
         color='Platform',
         legend=False, 
         use_container_width=True)
@@ -83,7 +84,7 @@ with c3:
     st.markdown('### Publisher Donut Chart')
     plost.donut_chart(
         data=publisher_sales,
-        theta='Sales',
+        theta='Global Sales (millions)',
         color='Publisher',
         legend=False, 
         use_container_width=True)
@@ -91,12 +92,12 @@ with c4:
     st.markdown('### Genre Donut Chart')
     plost.donut_chart(
         data=genre_sales,
-        theta='Sales',
+        theta='Global Sales (millions)',
         color='Genre',
         legend=False, 
         use_container_width=True)
     
 st.markdown('### Market Sales Over Time')
-st.line_chart(market_vg_sales, x = 'Year', y = ['NA_Sales', 'EU_Sales', 'JP_Sales', 'Other_Sales'])
+st.line_chart(market_vg_sales, x = 'Year', y = ['NA_Sales (millions)', 'EU_Sales (millions)', 'JP_Sales (millions)', 'Other_Sales (millions)'])
 
 
