415f5907fc0132abddb528622db05ccd800b3d08
JahanzebMK <j.khan360@outlook.com>
2023-12-27 07:10:05 +0000

    Add Predictor app

--- pages/Predictor.py
+++ pages/Predictor.py
@@ -1,3 +1,5 @@
+# Import important modules
+import streamlit as st
 import pickle
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.neural_network import MLPRegressor
@@ -6,7 +8,11 @@ import pandas as pd
 import warnings
 warnings.filterwarnings('ignore')
 
-print('This app wil predict the sales of your video game based on its name, publisher, year, platform and genre.')
+# Config layout of page and add important info
+st.set_page_config(layout='wide', initial_sidebar_state='expanded')
+st.sidebar.header('Video Game Sales Prediction')
+st.sidebar.subheader('By Jahanzeb Khan')
+st.sidebar.markdown('Enter the details of your video game to receive a prediction of their physical sales.')
 
 # Load in model, scaler and other necessary varaibles from training the MLPRegressor model in ML Notebook
 model = pickle.load(open('Pickle/model.sav', 'rb'))
@@ -15,13 +21,17 @@ predictor_df =  pd.read_pickle('Pickle/predictor_df.sav')
 with open('Pickle/franchise_list.pkl', 'rb') as f:
     franchise_list = pickle.load(f)
 
-
 # Take user inputs for their video game and process as necessary
-name = unidecode((str(input('Enter the name of your video game:')).lower()))
-publisher = unidecode(str(input('Enter the publisher of your video game:')).lower())
-year = scaler.transform([[int(input('Enter the year of your video game:'))]])
-platform = unidecode(str(input('Enter the platform of your video game:')).lower())
-genre = unidecode(str(input('Enter the genre of your video game:')).lower())
+name = st.text_input('Enter the name of your video game:')
+name = unidecode(str(name).lower())
+publisher = st.text_input('Enter the publisher of your video game:')
+publisher = unidecode(str(publisher).lower())
+year = st.number_input('Enter the year of your video game:', min_value=1980, value = 2024)
+year = scaler.transform([[int(year)]])
+platform = st.text_input('Enter the platform of your video game:')
+platform = unidecode(str(platform).lower())
+genre = st.text_input('Enter the genre of your video game:')
+genre = unidecode(str(genre).lower())
 
 # Loop through list of franchises and check if user video game belongs to any
 for item in franchise_list:
@@ -53,15 +63,7 @@ jp = round(predictions[0,2], 2)
 other = round(predictions[0,3], 2)
 total = round((predictions[0,0]+predictions[0,1]+predictions[0,2]+predictions[0,3]), 2)
 
-# Display predictions to users
-print('Sales will be shown in millions')
-print(f'Total Sales: {total}')
-print(f'NA Sales: {na}')
-print(f'EU Sales: {eu}')
-print(f'JP Sales: {jp}')
-print(f'Other Sales: {other}')
-   
-
-
-
-
+# Display predictions to user
+display_data = {'          ': ['Sales (Millions)'], 'North America': [na], 'Europe': [eu], 'Japan': [jp], 'Other': [other], 'Global': [total]}
+display_df = pd.DataFrame(data=display_data)
+st.dataframe(display_df,hide_index=True)
